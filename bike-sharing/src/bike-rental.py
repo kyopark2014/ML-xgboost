5791826fe87aad79bf04786466a9b2bfbc4b19d1
John Park <52392004+kyopark2014@users.noreply.github.com>
2022-10-24 08:09:48 +0900

    Update bike-rental.py

---
+++ bike-sharing/src/bike-rental.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import pickle
+import xgboost as xgb
+import argparse
+import pandas as pd
+import json
+
+import pandas as pd
+pd.options.display.max_rows=20
+pd.options.display.max_columns=10
