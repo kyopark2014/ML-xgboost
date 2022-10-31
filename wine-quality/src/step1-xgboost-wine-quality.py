f00d2239371a7de82156767215eb8e44854cb2fe
ksdyb.park <ksdyb@amazon.com>
2022-11-01 00:02:25 +0900

    update

---
+++ wine-quality/src/step1-xgboost-wine-quality.py
@@ -0,0 +1,108 @@
+import numpy as np
+import pandas as pd
+import time
+
+df = pd.read_csv('../data/wine_concat.csv')
+
+y = df['quality']
+X = df.drop(['quality'], axis=1)
+
+X = pd.get_dummies(X)  
+
+# Utility function to report best scores
+def report(results, n_top=3):
+    for i in range(1, n_top + 1):
+        candidates = np.flatnonzero(results["rank_test_score"] == i)
+        for candidate in candidates:
+            print("Model with rank: {0}".format(i))
+            print(
+                "Mean validation score(=RMSE): {0:.3f}".format(
+                    np.sqrt(-results["mean_test_score"][candidate]),
+                )
+            )
+            print("Parameters: {0}".format(results["params"][candidate]))
+            print("")
+
+
+from bayes_opt import BayesianOptimization
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+# Split train/test dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=2)
+
+# HPO: Bayesian Optimization
+def xgbc_cv(n_estimators, learning_rate, max_depth, gamma, min_child_weight, subsample, colsample_bytree):
+    min_rmse = 1e10
+
+    xgb = XGBRegressor(booster='gbtree', objective='reg:squarederror',
+                        n_estimators=int(n_estimators),
+                        learning_rate=learning_rate,
+                        max_depth=int(max_depth),
+                        gamma=gamma,
+                        min_child_weight=min_child_weight,
+                        subsample=subsample,
+                        colsample_bytree=colsample_bytree,
+                        random_state=2, verbosity=0, use_label_encoder=False, n_jobs=-1)
+
+    xgb.fit(X_train, y_train)    
+
+    y_pred = xgb.predict(X_test)
+
+    reg_mse = mean_squared_error(y_test, y_pred)
+    reg_rmse = np.sqrt(reg_mse)
+
+    print('RMSE: %0.3f' % (reg_rmse))   
+    
+    return -reg_rmse
+
+hyperparameter_space = {
+    'n_estimators': (50, 800),
+    'learning_rate': (0.01, 1.0),
+    'max_depth': (1, 8),
+    'gamma' : (0.01, 1),
+    'min_child_weight': (1, 20),
+    'subsample': (0.5, 1),
+    'colsample_bytree': (0.1, 1)
+}
+
+start = time.time()
+
+optimizer = BayesianOptimization(f=xgbc_cv, pbounds=hyperparameter_space, random_state=2, verbose=0)
+optimizer.maximize(init_points=5, n_iter=20, acq='ei')
+
+print('Elapsed time: %0.2fs' % (time.time()-start))   
+print(optimizer.max)
+
+# evaluation
+best_params = optimizer.max['params']
+
+print(best_params)
+
+model = XGBRegressor(booster='gbtree', objective='reg:squarederror',
+                        n_estimators=int(best_params['n_estimators']), 
+                        learning_rate=best_params['learning_rate'], 
+                        max_depth=int(best_params['max_depth']), 
+                        gamma=best_params['gamma'], 
+                        min_child_weight=int(best_params['min_child_weight']), 
+                        subsample=best_params['subsample'], 
+                        colsample_bytree=best_params['colsample_bytree'],
+                        random_state=2, verbosity=0, use_label_encoder=False, n_jobs=-1)
+
+model.fit(X_train, y_train)    
+
+y_pred = model.predict(X_test)
+
+reg_mse = mean_squared_error(y_test, y_pred)
+reg_rmse = np.sqrt(reg_mse)
+
+print('Elapsed time: %0.2fs' % (time.time()-start))        
+print('RMSE: %0.3f' % (reg_rmse))  
+
+model_name = "../output/xgboost_wine_quality.json"
+
+model.save_model(model_name)
+
+
+
