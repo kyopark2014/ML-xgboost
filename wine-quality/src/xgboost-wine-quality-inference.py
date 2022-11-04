d0fe48cbc04b7faaa8853dc7ccb86d1b9539d8bc
ksdyb.park <ksdyb@amazon.com>
2022-11-04 21:15:06 +0900

    update

--- wine-quality/src/xgboost-wine-quality-inference.py
+++ wine-quality/src/xgboost-wine-quality-inference.py
@@ -1,36 +1,47 @@
 import numpy as np
 import pandas as pd
 import time
-
 from xgboost import XGBRegressor
+
+# load model    
+model = XGBRegressor()
+model_name = "../output/xgboost_wine_quality.json"    
+model.load_model(model_name)
+    
+def handler(data): 
+    start = time.time()
+
+    # inference
+    results = model.predict(data)
+    print('result:', results)
+
+    return {
+        'statusCode': 200,
+        'body': results.tolist()
+    }
 
 def load_samples():
     data = pd.DataFrame(pd.read_json('../data/samples.json'))
-
     print('samples:', data)
-
     return data
 
 def main():
     start = time.time()
-
-    # load model
-    
-    model = XGBRegressor()
-
-    model_name = "../output/xgboost_wine_quality.json"    
-    model.load_model(model_name)
     
     # load samples
-    data = load_samples()
+    event = load_samples()
     
-    # inference
-    results = model.predict(data)
+    # Inference
+    results = handler(event)  
 
-    print(results)
+    # results
+    print(results['statusCode'])
+    print(results['body'])
+
     print('Elapsed time: %0.2fs' % (time.time()-start))   
 
 if __name__ == '__main__':
     main()
+
 
 
